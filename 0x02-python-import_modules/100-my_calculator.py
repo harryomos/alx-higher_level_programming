ca07d6b052b44e646f0ec99b579c2bdbdc4e1016
harryomos <esedharry@gmail.com>
2022-09-22 14:56:48 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
-    import sys
-
-    num_args = len(sys.argv)
-    if num_args != 4:
+    import calculator_1 as calc
+    from sys import argv, exit
+    argc = len(argv) - 1
+    operators = "+-*/"
+    if argc != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    a = int(sys.argv[1])
-    op = sys.argv[2]
-    b = int(sys.argv[3])
-
-    if op is '+':
-        print("{} {} {} = {}".format(a, op, b, add(a, b)))
-    elif op is '-':
-        print("{} {} {} = {}".format(a, op, b, sub(a, b)))
-    elif op is '*':
-        print("{} {} {} = {}".format(a, op, b, mul(a, b)))
-    elif op is '/':
-        print("{} {} {} = {}".format(a, op, b, div(a, b)))
+    if not argv[2] in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    a = (int)(argv[1])
+    b = (int)(argv[3])
+    operator = f"{argv[2]}"
+    if operator == "+":
+        res = calc.add(a, b)
+    elif operator == "-":
+        res = calc.sub(a, b)
+    elif operator == "*":
+        res = calc.mul(a, b)
+    elif operator == "/":
+        res = calc.div(a, b)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
+    print(f"{a:d} {argv[2]} {b:d} = {res}")

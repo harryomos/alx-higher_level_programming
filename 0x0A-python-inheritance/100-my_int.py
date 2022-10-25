2bb43c65744104e56c9e37941be10fd8a0764cdb
harryomos <esedharry@gmail.com>
2022-10-24 19:10:48 -0700

    100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
 """
-Contains the class MyInt
+Contains definition of class MyInt
 """
 
 
 class MyInt(int):
-    """rebel version of an integer, perfect for opposite day!"""
-    def __new__(cls, *args, **kwargs):
-        """create a new instance of the class"""
-        return super(MyInt, cls).__new__(cls, *args, **kwargs)
+    """Definition of class MyInt that inherits from class int"""
 
     def __eq__(self, other):
-        """what was != is now =="""
-        return int(self) != other
+        """Overrides equals, inverting it"""
+        return int(self) != int(other)
 
     def __ne__(self, other):
-        """what was == is now !="""
-        return int(self) == other 
+        """Overrides not-equals, inverting it"""
+        return int(self) == int(other) 
